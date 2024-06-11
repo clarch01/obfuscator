fce5fd0ac8ca68dc6bacc0ed79c27dc1dcae0950
Carlos <charles.larcher@hotmail.co.uk>
2024-06-11 10:00:21 +0100

    obfuscator mpv functioning with example data - untested and not pep-8

--- src/obfuscator.py
+++ src/obfuscator.py
@@ -3,6 +3,7 @@ import pandas as pd
 import boto3
 from pprint import pprint
 from io import StringIO
+import logging
 
 from redactor import redactor
 from s3_url_splitter import s3_url_splitter
@@ -10,40 +11,39 @@ from s3_url_splitter import s3_url_splitter
 def obfuscator(input):
     client = boto3.client('s3')
 
-
-    # split input 'file_to_obfuscate' into bucket and key values
-    url = s3_url_splitter(input['file_to_obfuscate'])
-    bucket = url['bucket']
-    key = url['key']
+# split input 'file_to_obfuscate' into bucket and key values
+    bucket_key = s3_url_splitter(input['file_to_obfuscate'])
 
 # get the object from bucket 
     response = client.get_object(
-        Bucket=url['bucket'],
-        Key=url['key']
+        Bucket=bucket_key['bucket'],
+        Key=bucket_key['key']
     )
-    file_bytestream = StringIO(response['Body'].read().decode('utf-8'))
+    file_bytestream = response['Body'].read().decode('utf-8')
 
-    # pprint(file_bytestream.read())
-    
-    df = pd.DataFrame(file_bytestream)
-    df.columns = df.iloc[0]
-    df = df[1:]
-    # dataframe is split into columns correctly for test case
-    pprint(df)
 # remove senesitive data
-    # data = pd.read_csv(input['file_to_obfuscate'])
-    # # column_names = list(data.columns)
-    # for field in input['pii_fields']:
-    #     new_column = data[field].apply(redactor)
-    #     data[field] = new_column
+    df = pd.read_csv(StringIO(file_bytestream))
 
-    # print(data)
+    for field in input['pii_fields']:
+        new_column = df.apply(redactor, axis=1)
+        # print(df['name'])
+        df[field] = new_column
+
+    upload_file = bytes(pd.DataFrame.to_csv(df), encoding='utf-8')
+
 # put new file back to s3 
+    upload_response = client.put_object(
+        Body=upload_file,
+        Bucket=bucket_key['bucket'],
+        Key=f"{bucket_key['key']}-obfuscated"
+    )
 
-test_input = {
-    "file_to_obfuscate": "s3://obfuscator-bucket/test-file",
-    "pii_fields": ["name", "email_address"]
-}
+    logging.info(upload_response)
 
-# list_buckets()
-obfuscator(test_input)
+# test_input = {
+#     "file_to_obfuscate": "s3://obfuscator-bucket/test-file",
+#     "pii_fields": ["name", "email_address"]
+# }
+
+# # list_buckets()
+# obfuscator(test_input)
--- src/pg.py
+++ src/pg.py
@@ -6,16 +6,25 @@ from list_buckets import list_buckets
 client = boto3.client('s3')
 
 # get file from s3 
-response = client.list_buckets()
+# response = client.list_objects(
+#     Bucket='obfuscator-bucket'
+# )
+# file = "s3://obfuscator-bucket/test-file-obfuscated"
 
-obj_response = client.list_objects(
-    Bucket = 'obfuscator-bucket'
+obj_response = client.get_object(
+    Bucket = 'obfuscator-bucket',
+    Key='test-file-obfuscated'
 )
+
+# print(obj_response)
+pprint(obj_response['Body'].read().decode('utf-8'))
+
+
 # pprint(response)
 # remove sensitive info
 # upload to s3
 
-file = "s3://obfuscator-bucket/test-file.csv"
+
 
 # split = re.split(r"//|/", file, maxsplit=2)
 # bucket = split[1]
@@ -31,4 +40,3 @@ file = "s3://obfuscator-bucket/test-file.csv"
 
 # print(list_buckets())
 
-pprint(obj_response['Contents'])
